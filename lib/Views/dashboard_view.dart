6595faaef357988ce5968a355139540d0bae441c
habibaaa682 <habibagalall2002@gmail.com>
2025-06-17 19:51:55 +0300

    assets-fonts

--- lib/Views/dashboard_view.dart
+++ lib/Views/dashboard_view.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/adaptive_Layout_Widget.dart';
+import '../widgets/dashboard_desktop_layout.dart';
+
 class DashboardView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return AdapativeLayoutWidget(
+      mobileLayout: (context) => SizedBox(),
+      tabletLayout:(context) => SizedBox(),
+      desktopLayout:(context) => DashBoardDeskTopLayout(),
+      );
   }
 }
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesCardReceive
+  /// assets/images/card-receive.svg
+  static const String assetsImagesCardReceive = "assets/images/card-receive.svg";
+
+  /// Assets for assetsImagesCardSend
+  /// assets/images/card-send.svg
+  static const String assetsImagesCardSend = "assets/images/card-send.svg";
+
+  /// Assets for assetsImagesCategory2
+  /// assets/images/category-2.svg
+  static const String assetsImagesCategory2 = "assets/images/category-2.svg";
+
+  /// Assets for assetsImagesChart2
+  /// assets/images/chart-2.svg
+  static const String assetsImagesChart2 = "assets/images/chart-2.svg";
+
+  /// Assets for assetsImagesConvertCard
+  /// assets/images/convert-card.svg
+  static const String assetsImagesConvertCard = "assets/images/convert-card.svg";
+
+  /// Assets for assetsImagesFrame
+  /// assets/images/Frame.svg
+  static const String assetsImagesFrame = "assets/images/Frame.svg";
+
+  /// Assets for assetsImagesGraph
+  /// assets/images/graph.svg
+  static const String assetsImagesGraph = "assets/images/graph.svg";
+
+  /// Assets for assetsImagesLogout
+  /// assets/images/logout.svg
+  static const String assetsImagesLogout = "assets/images/logout.svg";
+
+  /// Assets for assetsImagesMask group
+  /// assets/images/Mask group.svg
+  static const String assetsImagesMaskgroup = "assets/images/Mask group.svg";
+
+  /// Assets for assetsImagesMoneys
+  /// assets/images/moneys.svg
+  static const String assetsImagesMoneys = "assets/images/moneys.svg";
+
+  /// Assets for assetsImagesSetting2
+  /// assets/images/setting-2.svg
+  static const String assetsImagesSetting2 = "assets/images/setting-2.svg";
+
+  /// Assets for assetsImagesWallet2
+  /// assets/images/wallet-2.svg
+  static const String assetsImagesWallet2 = "assets/images/wallet-2.svg";
+}
+
